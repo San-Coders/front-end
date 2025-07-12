b02a0d871619c38e6e128e6aa5307233fc5cd60e
Sanesu <118468259+SanesuOff@users.noreply.github.com>
2025-07-12 20:18:30 +0200

    Typography + Firebase
    Added Firebase hosting configuration files (.firebaserc, firebase.json, .firebase/hosting.b3V0.cache) and initialized Firebase SDK in the project. Updated dependencies in package.json and package-lock.json to include Firebase and clsx. Added src/config/firebase-config.js for Firebase setup, updated index.tsx to use Firebase, and made related updates to styles and Tailwind config. Introduced a new typography component in the design system.

---
+++ src/config/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChUCmyrLkNEaD9906EOh5NNIbn_IlKDw4",
+  authDomain: "sancodersx.firebaseapp.com",
+  projectId: "sancodersx",
+  storageBucket: "sancodersx.firebasestorage.app",
+  messagingSenderId: "78249168148",
+  appId: "1:78249168148:web:f4e9969b95105bcd3b4601",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,31 @@
 import { Seo } from "@/ui/components/seo/seo";
+import { Typography } from "@/ui/design-system/typography/typography";
 
 export default function Home() {
   return (
     <>
       <Seo title="San Coders" description="Description..." />
-      <h1>Hello World</h1>
+
+      <div className="space-y-5">
+        <Typography variant="display" component="div">
+          San Coders
+        </Typography>
+        <Typography variant="body-lg" component="h1" theme="primary">
+          San Coders
+        </Typography>
+        <Typography variant="lead" component="div" theme="secondary">
+          San Coders
+        </Typography>
+        <Typography variant="body-sm" component="div" theme="gray">
+          San Coders
+        </Typography>
+        <Typography variant="caption4" component="div">
+          San Coders
+        </Typography>
+        <Typography variant="caption4" component="div" weight="medium">
+          San Coders
+        </Typography>
+      </div>
     </>
   );
 }
