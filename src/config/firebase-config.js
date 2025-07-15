24b6fe7a5e199504e89289373fe2d606b0d1f94e
Sanesu <118468259+SanesuOff@users.noreply.github.com>
2025-07-16 00:03:43 +0200

    Update Firebase config to use environment variables
    Replaced hardcoded Firebase configuration values in src/config/firebase-config.js with environment variable references. Updated package.json dependencies, and regenerated package-lock.json to reflect these changes. This improves security and flexibility for different deployment environments.

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,12 +1,14 @@
 import { initializeApp } from "firebase/app";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+	apiKey: process.env.apiKey,
+	authDomain: process.env.authDomain,
+	projectId: process.env.projectId,
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.messagingSenderId,
+	appId: process.env.appId,
 };
 
 // Initialize Firebase
