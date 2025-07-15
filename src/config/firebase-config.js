db3cd4d5996c7e58c059697c0ef57e5cce5640a8
Sanesu <118468259+SanesuOff@users.noreply.github.com>
2025-07-15 23:40:30 +0200

    Update firebase-config.js

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyChUCmyrLkNEaD9906EOh5NNIbn_IlKDw4",
-  authDomain: "sancodersx.firebaseapp.com",
-  projectId: "sancodersx",
-  storageBucket: "sancodersx.firebasestorage.app",
-  messagingSenderId: "78249168148",
-  appId: "1:78249168148:web:f4e9969b95105bcd3b4601",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
