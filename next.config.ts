d66f95976fee3ed284ac5d018f48d4544946e852
Sanesu <118468259+SanesuOff@users.noreply.github.com>
2025-07-15 23:27:21 +0200

    Add Prettier config, update docs, and set Firebase env
    Added a .prettierrc file for code formatting standards. Expanded the README.md with detailed setup and usage instructions in French. Updated next.config.ts to include Firebase environment variables. Added author field to package.json.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: false,
-  images: { unoptimized: true },
-  output: "export",
-  distDir: "out",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+	/* config options here */
+	reactStrictMode: false,
+	images: { unoptimized: true },
+	output: "export",
+	distDir: "out",
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+
+	// Your web app's Firebase configuration
+	env: {
+		API_KEY: "",
+		AUTH_DOMAIN: "",
+		PROJECT_ID: "",
+		STORAGE_BUCKET: "",
+		MESSAGING_SENDER_ID: "",
+		APP_ID: "",
+	},
 };
 
 export default nextConfig;
