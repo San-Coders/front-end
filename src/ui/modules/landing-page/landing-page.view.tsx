ef3a42307a675a5ceb19124a2aa3e3df25ebc685
Sanesu <118468259+SanesuOff@users.noreply.github.com>
2025-07-16 00:47:45 +0200

    Add Slack section and social buttons to landing page
    Introduces a new SlackView component to promote joining the Slack group, and integrates it into the landing page. Also adds SocialNetworksButton to the featured section for improved social media visibility and updates layout for better structure.

--- src/ui/modules/landing-page/landing-page.view.tsx
+++ src/ui/modules/landing-page/landing-page.view.tsx
@@ -1,11 +1,13 @@
 import { HeroTopView } from "./components/hero-top/hero-top.view";
 import { FeaturedView } from "./components/featured/featured.view";
+import { SlackView } from "./components/slack/slack";
 
 export const LandingPageView = () => {
-  return (
-    <>
-      <HeroTopView />
-      <FeaturedView />
-    </>
-  );
+	return (
+		<>
+			<HeroTopView />
+			<FeaturedView />
+			<SlackView />
+		</>
+	);
 };
